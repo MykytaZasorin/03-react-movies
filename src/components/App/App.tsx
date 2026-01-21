309368d6e8b2a0bbce4066b418bbc564e44a1af9
Mykyta_zn <mykytazasorin@gmail.com>
2026-01-21 19:15:19 +0200

    upd

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-import type { Movie } from "../types/movie";
-import { fetchMovies } from "../services/MovieService";
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/MovieService";
 
-import SearchBar from "../components/SearchBar/SearchBar";
-import MovieGrid from "../components/MovieGrid/MovieGrid";
-import Loader from "../components/Loader/Loader";
-import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
-import MovieModal from "../components/MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
